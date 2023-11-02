f9f8f3c163b7bc4e2b794a3be9f15704b3d64a95
anhducbui6103 <anhduc.bui.6103@gmail.com>
2023-11-03 02:42:07 +0700

    Fix dp_tsp.cpp

--- dp_tsp.cpp
+++ dp_tsp.cpp
@@ -24,9 +24,48 @@ int dp(int i, int S)
     {
         if (S & (1 << j))
             continue;
-        res = min(res, C[i][j] + dp(j, S | (i << j)));
+        res = min(res, C[i][j] + dp(j, S | (1 << j)));
     }
     return mem[i][S] = res;
+}
+
+void trace(int i, int S)
+{
+    cout << i << " -> ";
+    if (S == (1 << n) - 1)
+        cout << 0 << endl;
+    for (int j = 0; j < n; j++)
+    {
+        if (S & (1 << j))
+            continue;
+        if (mem[i][S] == C[i][j] + mem[j][S | (1 << j)])
+        {
+            trace(j, S | (1 << j));
+            break;
+        }
+    }
+}
+
+void iter_trace()
+{
+    int i = 0, S = 1;
+    cout << 0 << " -> ";
+    for (int k = 1; k < n; k++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            if (S & (1 << j))
+                continue;
+            if (mem[i][S] == C[i][j] + mem[j][S | (1 << j)])
+            {
+                i = j;
+                S |= (1 << j);
+                break;
+            }
+        }
+        cout << i << " -> ";
+    }
+    cout << 0;
 }
 
 int main()
@@ -38,6 +77,7 @@ int main()
 
     input();
     cout << dp(0, 1) << endl;
-
+    // trace(0, 1);
+    iter_trace();
     return 0;
 }
