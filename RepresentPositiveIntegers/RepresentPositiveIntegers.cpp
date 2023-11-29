c7d420863d84395c38c50cc817aff309d7e7a292
anhducbui6103 <anhduc.bui.6103@gmail.com>
2023-11-29 16:04:47 +0700

    Add RepresentationPositiveIntegers

--- RepresentPositiveIntegers/RepresentPositiveIntegers.cpp
+++ RepresentPositiveIntegers/RepresentPositiveIntegers.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+const int MOD = 1e9+7;
 
-int countWays(int n) {
+int countWays(int n)
+{
     vector<int> dp(n + 1, 0);
     dp[0] = 1;
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = i; j <= n; j++) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i; j <= n; j++)
+        {
             dp[j] += dp[j - i];
+            dp[j] %= MOD;
         }
     }
 
     return dp[n];
 }
 
-int main() {
+int main()
+{
     int n;
     cin >> n;
 
     int ways = countWays(n);
-    cout << ways <<endl;
+    cout << ways << endl;
 
     return 0;
 }
