87e7a122ac08132b864cff4cbd1a211f75b8e68e
anhducbui6103 <anhduc.bui.6103@gmail.com>
2023-11-29 16:59:57 +0700

    Update MaxDistance

--- MaxDistance/MaxDistance.cpp
+++ MaxDistance/MaxDistance.cpp
@@ -1,30 +1,43 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int number_test_case;
-int const MAX = 1e5 + 5;
-int n, c;
-int x[MAX];
-
-void input()
-{
-    cin >> n >> c;
-    for (int i = 0; i < n; i++)
-        cin >> x[i];
+bool check(int mid, vector<int> &x, int c) {
+    int count = 1;
+    int last = x[0];
+    for (int i = 1; i < x.size(); i++) {
+        if (x[i] - last >= mid) {
+            last = x[i];
+            count++;
+        }
+    }
+    return count >= c;
 }
 
-int PrintMaxDistance(){}
-
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
-    cin >> number_test_case;
-    for (int v = 0; v < number_test_case; v++)
-    {
-        input();
-        cout << n;
+void solve() {
+    int n, c;
+    cin >> n >> c;
+    vector<int> x(n);
+    for (int i = 0; i < n; i++)
+        cin >> x[i];
+    sort(x.begin(), x.end());
+    int l = 0;
+    int r = x[n-1] - x[0] + 1;
+    while (r - l > 1) {
+        int mid = (l + r) / 2;
+        if (check(mid, x, c))
+            l = mid;
+        else
+            r = mid;
     }
+    cout << l << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
 }
